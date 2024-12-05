30e95b8602699a95a47a0b45411b0bb927bc42bc
YHSifat <148780601+YHSifat@users.noreply.github.com>
2024-12-05 21:44:22 +0600

    template added

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mxx 200005
+#define answ ((out)?"YES":"NO")
+#define endl '\n'
+
+
+void solve(int tc){
+    
+}
+
+
+void sub_run(){
+    freopen("inp.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+}
+
+
+int main(){
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    int t=1;
+    sub_run();  //MAKE SURE TO COMMENT THIS LINE BEFORE SUBMITTING
+    cin>>t;
+    for(int i=1;i<=t;i++)solve(i);
+}
